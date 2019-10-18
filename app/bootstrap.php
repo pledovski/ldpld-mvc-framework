5f6e683d615d86ed9ac538e409ec818618d2a9be
pledovski <human.pled@gmail.com>
2019-10-18 17:58:47 +0300

    Header & Footer Includes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
-  // Load libraries
-  require_once 'libraries/Core.php';
-  require_once 'libraries/Controller.php';
-  require_once 'libraries/Database.php';
+  // Load Cnfig
+  require_once 'config/config.php';
+
+  // Autoload Core Libraries
+  spl_autoload_register(function ($className) {
+    require_once 'libraries/' . $className .'.php';
+  });
---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://192.168.64.3/ldpldmvc');
+  // Site Name
+  define('SITENAME', 'ldpldMVC');
