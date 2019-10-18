200efc6dd687cd40b002c87d354657e30e795e3b
pledovski <human.pled@gmail.com>
2019-10-18 14:58:45 +0300

    Bootstrap FIle & Core Class

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,19 +1,27 @@
 <?php
-  /*
+/*
    * App Core Class
    * Creates URL & loads core controller
    * URL FORMAT - /controller/method/params
-   */ 
-  class Core {
-    protected $currentController = 'Pages';
-    protected $currentMethod = 'index';
-    protected $params = [];
+   */
+class Core
+{
+  protected $currentController = 'Pages';
+  protected $currentMethod = 'index';
+  protected $params = [];
 
-    public function __construct() {
-      $this->getUrl();
-    }
+  public function __construct()
+  {
+    print_r($url = $this->getUrl());
+  }
 
-    public function getUrl() {
-      echo $_GET['url'];
+  public function getUrl()
+  {
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
     }
   }
+}
