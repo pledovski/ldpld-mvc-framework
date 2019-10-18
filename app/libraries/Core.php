00481bb8f134bbb68aa026aa4e42bb539fba87a7
pledovski <human.pled@gmail.com>
2019-10-18 15:07:16 +0300

    Internediate

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -12,7 +12,18 @@ class Core
 
   public function __construct()
   {
-    print_r($url = $this->getUrl());
+    // print_r($url = $this->getUrl());
+
+    $url = $this->getUrl();
+
+    // Look in controllers for first value
+    if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+      // If exists, set as controller
+      $this->currentController = ucwords($url[0]);
+
+      //Unset 0 index
+      unset($url[0]);
+    }
   }
 
   public function getUrl()
